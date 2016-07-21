71a01fa081434526fd470a27047a6418d9999f8c
cshupp <cshupp@gmail.com>
2016-07-21 13:30:02 -0400

    removing extraneous logging.

--- db-config-builder/src/main/java/gov/vha/isaac/ochre/pombuilder/upload/SrcUploadCreator.java
+++ db-config-builder/src/main/java/gov/vha/isaac/ochre/pombuilder/upload/SrcUploadCreator.java
@@ -50,14 +50,6 @@ public class SrcUploadCreator
 	private static final Logger LOG = LogManager.getLogger();
 	
 	
-	public static void logCrap() {
-		LOG.debug("Hi Cris");
-		LOG.info("It is a nice day.");
-		LOG.warn("It is a rainy day");
-		LOG.error("It is a scary day");
-		LOG.fatal("Armegeddon is upon us.");//Some test logging to integrate with rails
-	}
-	
 	/**
 	 * @param uploadType - What type of content is being uploaded.
 	 * @param version - What version number does the passed in content represent
@@ -82,12 +74,6 @@ public class SrcUploadCreator
 			String gitRepositoryURL, String gitUsername, String gitPassword,
 			String artifactRepositoryURL, String repositoryUsername, String repositoryPassword) throws Throwable
 	{
-		LOG.debug("Hi Cris");
-		LOG.info("It is a nice day.");
-		LOG.warn("It is a rainy day");
-		LOG.error("It is a scary day");
-		LOG.fatal("Armegeddon is upon us.");//Some test logging to integrate with rails
-		
 		LOG.info("Building the task to create a source upload configuration...");
 
 		if (filesToUpload == null || filesToUpload.size() == 0)
