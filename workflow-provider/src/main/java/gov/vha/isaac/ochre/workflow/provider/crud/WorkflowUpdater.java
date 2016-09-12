500000e08d8ae2a4d268c04a41c72048913e7a5a
jefron <jefron@westcoastInformatics.com>
2016-09-12 15:43:01 -0700

    Add ability to add components via CommitRecord

--- workflow-provider/src/main/java/gov/vha/isaac/ochre/workflow/provider/crud/WorkflowUpdater.java
+++ workflow-provider/src/main/java/gov/vha/isaac/ochre/workflow/provider/crud/WorkflowUpdater.java
@@ -20,6 +20,8 @@ package gov.vha.isaac.ochre.workflow.provider.crud;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Optional;
+import java.util.PrimitiveIterator.OfInt;
 import java.util.Set;
 import java.util.UUID;
 
@@ -28,6 +30,8 @@ import gov.vha.isaac.metacontent.workflow.contents.AvailableAction;
 import gov.vha.isaac.metacontent.workflow.contents.ProcessDetail;
 import gov.vha.isaac.metacontent.workflow.contents.ProcessHistory;
 import gov.vha.isaac.metacontent.workflow.contents.UserPermission;
+import gov.vha.isaac.ochre.api.Get;
+import gov.vha.isaac.ochre.api.commit.CommitRecord;
 import gov.vha.isaac.ochre.api.metacontent.workflow.StorableWorkflowContents.ProcessStatus;
 import gov.vha.isaac.ochre.workflow.provider.AbstractWorkflowUtilities;
 
@@ -39,6 +43,8 @@ import gov.vha.isaac.ochre.workflow.provider.AbstractWorkflowUtilities;
  * @author <a href="mailto:jefron@westcoastinformatics.com">Jesse Efron</a>
  */
 public class WorkflowUpdater extends AbstractWorkflowUtilities {
+
+	static private UUID restTestProcessId;
 
 	/**
 	 * Instantiates a new workflow updater.
@@ -75,8 +81,7 @@ public class WorkflowUpdater extends AbstractWorkflowUtilities {
 	 * @throws Exception
 	 *             the exception
 	 */
-	public UUID advanceWorkflow(UUID processId, int userNid, String actionRequested, String comment)
-			throws Exception {
+	public UUID advanceWorkflow(UUID processId, int userNid, String actionRequested, String comment) throws Exception {
 		WorkflowAccessor wfAccessor = new WorkflowAccessor(store);
 
 		// Get User Permissible actions
@@ -87,45 +92,44 @@ public class WorkflowUpdater extends AbstractWorkflowUtilities {
 		for (AvailableAction action : userPermissableActions) {
 			if (action.getAction().equals(actionRequested)) {
 				ProcessDetail process = processDetailStore.getEntry(processId);
-				
+
 				// Update Process Details for launch, cancel, or conclude
 				if (getEndWorkflowTypeMap().get(EndWorkflowType.CANCELED).contains(actionRequested)) {
 					// Request to cancel workflow
 					WorkflowProcessInitializerConcluder initConcluder = new WorkflowProcessInitializerConcluder(store);
-					initConcluder.endWorkflowProcess(processId, action, userNid, comment,
-							EndWorkflowType.CANCELED);
+					initConcluder.endWorkflowProcess(processId, action, userNid, comment, EndWorkflowType.CANCELED);
 				} else if (process.getStatus().equals(ProcessStatus.DEFINED)) {
 					for (AvailableAction startAction : getDefinitionStartActionMap().get(process.getDefinitionId())) {
 						if (startAction.getOutcomeState().equals(action.getInitialState())) {
-        					// Advancing request is to launch workflow
-        					WorkflowProcessInitializerConcluder initConcluder = new WorkflowProcessInitializerConcluder(store);
-        					initConcluder.launchWorkflowProcess(processId);
-        					
-        					break;
-						}						
+							// Advancing request is to launch workflow
+							WorkflowProcessInitializerConcluder initConcluder = new WorkflowProcessInitializerConcluder(
+									store);
+							initConcluder.launchWorkflowProcess(processId);
+
+							break;
+						}
 					}
 				} else if (getEndWorkflowTypeMap().get(EndWorkflowType.CONCLUDED).contains(action)) {
 					// Conclude Request made
 					WorkflowProcessInitializerConcluder initConcluder = new WorkflowProcessInitializerConcluder(store);
-					initConcluder.endWorkflowProcess(processId, action, userNid, comment,
-							EndWorkflowType.CONCLUDED);
+					initConcluder.endWorkflowProcess(processId, action, userNid, comment, EndWorkflowType.CONCLUDED);
 				}
 
 				if (getEndWorkflowTypeMap().get(EndWorkflowType.CANCELED).contains(action)) {
-					// Special case where comment added to cancel screen and cancel store
+					// Special case where comment added to cancel screen and
+					// cancel store
 					// TODO: Better approach?
 					comment = getCanceledComment();
 				}
 
 				// Add to process history
 				ProcessHistory entry = new ProcessHistory(processId, userNid, new Date().getTime(),
-						action.getInitialState(), action.getAction(),
-						action.getOutcomeState(), comment);
+						action.getInitialState(), action.getAction(), action.getOutcomeState(), comment);
 				return processHistoryStore.addEntry(entry);
 			}
 		}
 
-	return null;
+		return null;
 
 	}
 
@@ -159,27 +163,6 @@ public class WorkflowUpdater extends AbstractWorkflowUtilities {
 		}
 
 		// TODO: Handle reverting automatically
-	}
-
-	public void addComponentToWorkflow(UUID processId, int compNid, int stampSeq) throws Exception {
-		if (compNid >= 0) {
-			// Ensure a Nid is passed in
-			throw new Exception("Component added must be a nid.  Nids must be negative.  Component added is not: " + compNid);
-		}
-
-		ProcessDetail detail = processDetailStore.getEntry(processId);
-
-		if (isProcessInAcceptableEditState(detail, compNid, "add")) {
-			if (detail.getComponentNidToStampsMap().containsKey(compNid)) {
-				detail.getComponentNidToStampsMap().get(compNid).add(stampSeq);
-			} else {
-				ArrayList<Integer> list = new ArrayList<>();
-				list.add(stampSeq);
-				detail.getComponentNidToStampsMap().put(compNid, list);
-			}
-
-			processDetailStore.updateEntry(processId, detail);
-		}
 	}
 
 	private boolean isProcessInAcceptableEditState(ProcessDetail detail, int compNid, String exceptionCase)
@@ -228,5 +211,52 @@ public class WorkflowUpdater extends AbstractWorkflowUtilities {
 		}
 
 		return true;
+	}
+
+	public void addCommitRecordToWorkflow(UUID processId, Optional<CommitRecord> commitRecord) throws Exception {
+		if (commitRecord.isPresent()) {
+			OfInt conceptItr = Get.identifierService()
+					.getConceptNidsForConceptSequences(commitRecord.get().getConceptsInCommit().parallelStream())
+					.iterator();
+			OfInt sememeItr = Get.identifierService()
+					.getSememeNidsForSememeSequences(commitRecord.get().getSememesInCommit().parallelStream())
+					.iterator();
+			OfInt stampItr = commitRecord.get().getStampsInCommit().getIntIterator();
+
+			while (stampItr.hasNext()) {
+				int stampSeq = stampItr.next();
+				while (conceptItr.hasNext()) {
+					addComponentToWorkflow(processId, conceptItr.next(), stampSeq);
+				}
+
+				while (sememeItr.hasNext()) {
+					addComponentToWorkflow(processId, sememeItr.next(), stampSeq);
+				}
+			}
+		}
+	}
+
+	public void addComponentToWorkflow(UUID processId, int compNid, int stampSeq) throws Exception {
+		ProcessDetail detail = processDetailStore.getEntry(processId);
+
+		if (isProcessInAcceptableEditState(detail, compNid, "add")) {
+			if (detail.getComponentNidToStampsMap().containsKey(compNid)) {
+				detail.getComponentNidToStampsMap().get(compNid).add(stampSeq);
+			} else {
+				ArrayList<Integer> list = new ArrayList<>();
+				list.add(stampSeq);
+				detail.getComponentNidToStampsMap().put(compNid, list);
+			}
+
+			processDetailStore.updateEntry(processId, detail);
+		}
+	}
+
+	public UUID getRestTestProcessId() {
+		return restTestProcessId;
+	}
+
+	public void setRestTestProcessId(UUID processId) {
+		restTestProcessId = processId;
 	}
 }
