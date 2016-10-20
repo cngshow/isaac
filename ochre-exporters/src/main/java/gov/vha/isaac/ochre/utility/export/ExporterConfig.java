f89106f9097c5a3829a3303b8d2d52ecb93c35f3
Neill Robins <neill.robins@vetsez.com>
2016-10-20 16:33:27 -0400

    Lots of work. <Types> and <Subsets> working now.

--- ochre-exporters/src/main/java/gov/vha/isaac/ochre/utility/export/ExporterConfig.java
+++ ochre-exporters/src/main/java/gov/vha/isaac/ochre/utility/export/ExporterConfig.java
@@ -2,21 +2,17 @@ package gov.vha.isaac.ochre.utility.export;
 
 import static gov.vha.isaac.ochre.api.constants.Constants.DATA_STORE_ROOT_LOCATION_PROPERTY;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.PrimitiveIterator;
-import java.util.Properties;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.TreeMap;
+import java.util.UUID;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -37,19 +33,48 @@ import gov.vha.isaac.MetaData;
 import gov.vha.isaac.ochre.api.ConfigurationService;
 import gov.vha.isaac.ochre.api.Get;
 import gov.vha.isaac.ochre.api.LookupService;
+import gov.vha.isaac.ochre.api.bootstrap.TermAux;
 import gov.vha.isaac.ochre.api.chronicle.LatestVersion;
+import gov.vha.isaac.ochre.api.chronicle.ObjectChronologyType;
 import gov.vha.isaac.ochre.api.collections.SememeSequenceSet;
 import gov.vha.isaac.ochre.api.component.concept.ConceptChronology;
+import gov.vha.isaac.ochre.api.component.concept.ConceptSpecification;
 import gov.vha.isaac.ochre.api.component.concept.ConceptVersion;
 import gov.vha.isaac.ochre.api.component.sememe.SememeChronology;
 import gov.vha.isaac.ochre.api.component.sememe.SememeType;
+import gov.vha.isaac.ochre.api.component.sememe.version.ComponentNidSememe;
 import gov.vha.isaac.ochre.api.component.sememe.version.DescriptionSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.DynamicSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.LogicGraphSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.LongSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.MutableComponentNidSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.MutableDescriptionSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.MutableDynamicSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.MutableLogicGraphSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.MutableLongSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.MutableStringSememe;
 import gov.vha.isaac.ochre.api.component.sememe.version.SememeVersion;
+import gov.vha.isaac.ochre.api.component.sememe.version.StringSememe;
+import gov.vha.isaac.ochre.api.component.sememe.version.dynamicSememe.DynamicSememeColumnInfo;
+import gov.vha.isaac.ochre.api.component.sememe.version.dynamicSememe.DynamicSememeColumnUtility;
+import gov.vha.isaac.ochre.api.component.sememe.version.dynamicSememe.DynamicSememeData;
+import gov.vha.isaac.ochre.api.component.sememe.version.dynamicSememe.DynamicSememeDataType;
+import gov.vha.isaac.ochre.api.component.sememe.version.dynamicSememe.DynamicSememeUsageDescription;
+import gov.vha.isaac.ochre.api.component.sememe.version.dynamicSememe.DynamicSememeUtility;
+import gov.vha.isaac.ochre.api.component.sememe.version.dynamicSememe.DynamicSememeValidatorType;
+import gov.vha.isaac.ochre.api.component.sememe.version.dynamicSememe.dataTypes.DynamicSememeArray;
+import gov.vha.isaac.ochre.api.component.sememe.version.dynamicSememe.dataTypes.DynamicSememeString;
+import gov.vha.isaac.ochre.api.constants.DynamicSememeConstants;
+import gov.vha.isaac.ochre.api.relationship.RelationshipVersionAdaptor;
 import gov.vha.isaac.ochre.api.util.ArtifactUtilities;
 import gov.vha.isaac.ochre.api.util.DBLocator;
 import gov.vha.isaac.ochre.api.util.DownloadUnzipTask;
 import gov.vha.isaac.ochre.api.util.WorkExecutors;
 import gov.vha.isaac.ochre.impl.utility.SimpleDisplayConcept;
+import gov.vha.isaac.ochre.model.configuration.StampCoordinates;
+import gov.vha.isaac.ochre.model.relationship.RelationshipAdaptorChronicleKeyImpl;
+import gov.vha.isaac.ochre.model.relationship.RelationshipVersionAdaptorImpl;
+import gov.vha.isaac.ochre.model.sememe.DynamicSememeUsageDescriptionImpl;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.concurrent.Task;
@@ -79,6 +104,12 @@ public class ExporterConfig {
 	
 	private boolean shutdown = false;
 	
+	private Map<UUID, String> designationTypes = new HashMap<>();
+	private Map<UUID, String> propertyTypes = new HashMap<>();
+	private Map<UUID, String> relationshipTypes = new HashMap<>();
+	private Map<String, List<String>> subsets = new TreeMap<>();
+	
+	
 	public static void main(String[] args) {
 
 		ExporterConfig ec = new ExporterConfig();
@@ -86,34 +117,6 @@ public class ExporterConfig {
 		ec.testXml();
 		
 		javafx.application.Platform.exit();
-		
-	}
-	
-	private void testXml() {
-		
-		try {
-			JAXBContext jc = JAXBContext.newInstance(Terminology.class);
-			Terminology tx = new Terminology();
-			
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			//To avoid XXE injections add setFeature
-			dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-	        DocumentBuilder db = dbf.newDocumentBuilder();
-	        Document document = db.newDocument();
-	        
-	        Marshaller marshaller = jc.createMarshaller();
-	        marshaller.marshal(tx, document);
-	        
-	        TransformerFactory tf = TransformerFactory.newInstance();
-	        //To protect a TransformerFactory against XXE injection add setAttribute
-	        tf.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-	        Transformer t = tf.newTransformer();
-	        DOMSource source = new DOMSource(document);
-	        StreamResult result = new StreamResult(System.out);
-	        t.transform(source, result);
-		} catch (Exception e) {
-			
-		}
 		
 	}
 	
@@ -179,59 +182,77 @@ public class ExporterConfig {
 			log.error("Failure starting ISAAC", e);
 		}
 			
-		// TODO
-		java.util.Map<Integer, java.util.UUID> conceptDescriptions = new java.util.HashMap<>();
+		// ISAAC Associations => RelationshipType UUID
+		UUID vhatAssociationTypesUUID = UUID.fromString("55f56c52-757a-5db8-bf1e-3ed613711386");
+		//int vhatAssociationTypesNid = Get.identifierService().getNidForUuids(vhatAssociationTypesUUID); //-2147481336
+		// Add to map
+		Get.taxonomyService().getAllRelationshipOriginSequences(
+				Get.identifierService().getNidForUuids(vhatAssociationTypesUUID)).forEach((conceptId) -> {
+			ConceptChronology<? extends ConceptVersion<?>> concept = Get.conceptService().getConcept(conceptId);
+			relationshipTypes.put(concept.getPrimordialUuid(), concept.getConceptDescriptionText());
+		});
+
+		// ISAAC Attributes => PropertyType UUID
+		UUID vhatPropertyTypesUUID = UUID.fromString("eb7696e7-fe40-5985-9b2e-4e3d840a47b7"); 
+		//int vhatPropertyTypesNid = Get.identifierService().getNidForUuids(vhatPropertyTypesUUID); //-2147481872 
+		// Add to map
+		Get.taxonomyService().getAllRelationshipOriginSequences(
+				Get.identifierService().getNidForUuids(vhatPropertyTypesUUID)).forEach((conceptId) -> {
+			ConceptChronology<? extends ConceptVersion<?>> concept = Get.conceptService().getConcept(conceptId);
+			propertyTypes.put(concept.getPrimordialUuid(), concept.getConceptDescriptionText());
+		});
+
+		// ISAAC Descriptions => DesignationType UUID
+		UUID vhatDesignationTypesUUID = UUID.fromString("09c43aa9-eaed-5217-bc5f-23cacca4df38"); 
+		//int vhatDesignationTypesNid = Get.identifierService().getNidForUuids(vhatDesignationTypesUUID); //-2147481914
+		// Add to map
+		Get.taxonomyService().getAllRelationshipOriginSequences(
+				Get.identifierService().getNidForUuids(vhatDesignationTypesUUID)).forEach((conceptId) -> {
+			ConceptChronology<? extends ConceptVersion<?>> concept = Get.conceptService().getConcept(conceptId);
+			designationTypes.put(concept.getPrimordialUuid(), concept.getConceptDescriptionText());
+		});
 		
-		Get.conceptService().getConceptChronologyStream()
-			.forEach((conceptChronology) -> {
-				System.out.println(conceptChronology.toUserString()+": ");
-				Get.sememeService().getSememesForComponent(conceptChronology.getNid())
-					.forEach((sememeChronology) -> {
-						System.out.print("     "+sememeChronology.getNid()+" <");
-						if (Get.sememeService().hasSememe(sememeChronology.getNid())) {
-							System.out.print(sememeChronology.getSememeType()+" ");
-							if (sememeChronology.getSememeType() == SememeType.DESCRIPTION) {
-								conceptDescriptions.put(sememeChronology.getNid(), sememeChronology.getPrimordialUuid());
-							}
-							/*Get.sememeService().getDescriptionsForComponent(sememeChronology.getNid())
-								.forEach((sememeDescription) -> {
-								System.out.print(((DescriptionSememe<?>)sememeDescription).getText());
-							});*/
-						}
-						System.out.println("> ");
-				});
-				System.out.println();
-	        });
-		System.out.println("Descriptions: " + conceptDescriptions.size());
-		
-		/*Get.sememeService().getAssemblageTypes().forEach(assemblageSeq -> {
-			System.out.println(assemblageSeq 
-								//+ ":" + Get.sememeService().getSememe(assemblageSeq).getNid() 
-								+ ":" + Get.sememeService().getSememe(assemblageSeq).toUserString()
-								//+ ":" + Get.sememeService().getSememe(assemblageSeq).getPrimordialUuid().toString()
-								//+ ":" + Get.sememeService().getSememe(assemblageSeq).getSememeType().toString()
-								+ "\n"
-								);
-		});*/
-		
-        System.out.println(Get.conceptService().getConceptChronologyStream().count());
-        
-        // Produces NPE
-        /*Get.sememeService().getSememeChronologyStream()
-		.forEach((sememeChronology) -> {
-			List<? extends SememeChronology<? extends SememeVersion<?>>> _sc = sememeChronology.getSememeList();
-			System.out.print(sememeChronology.getSememeType() + ": ");
-			if (_sc.size() > 0) {
-				for (SememeChronology<? extends SememeVersion<?>>__sc : _sc) {
-					System.out.print(__sc.getNid()+" ");
-				}
-				System.out.println();
+		// ISAAC VHAT Refsets => Subsets UUID
+		UUID vhatRefsetsUUID = UUID.fromString("99173138-dcaa-5a77-a4eb-311b01991b88");
+		//int vhatRefsetsNid = Get.identifierService().getNidForUuids(vhatRefsetsUUID); //-2147481162
+		// Get data, Add to map
+		Get.taxonomyService().getAllRelationshipOriginSequences(Get.identifierService().getNidForUuids(vhatRefsetsUUID)).forEach((tcs) -> {
+			ConceptChronology<? extends ConceptVersion<?>> concept = Get.conceptService().getConcept(tcs);
+			// Excluding these:
+			// 		All VHAT Concepts / UUID: f2df3cf5-a426-50f9-a660-081a5ca22c70 / Nid: -2147480814
+			// 		Missing SDO Code System Concepts / UUID: 52460eeb-1388-512d-a5e4-fddd64fe0aee / Nid: -2146188289
+			if (concept.getPrimordialUuid() == UUID.fromString("f2df3cf5-a426-50f9-a660-081a5ca22c70") 
+				|| 	concept.getPrimordialUuid() == UUID.fromString("52460eeb-1388-512d-a5e4-fddd64fe0aee")) {
+				// Skip
 			} else {
-				System.out.println(sememeChronology.getNid());
+				// VUID UUID = f31f3d89-5a6f-5e1f-81d3-5b68344d96f9
+				Get.sememeService().getSememesForComponent(concept.getNid()).forEach((sememe) -> {
+					if (sememe.getSememeType() == SememeType.STRING) {
+						@SuppressWarnings({ "rawtypes", "unchecked" })
+		                Optional<LatestVersion<? extends StringSememe>> sememeVersion
+		                		= ((SememeChronology) sememe).getLatestVersion(StringSememe.class, StampCoordinates.getDevelopmentLatestActiveOnly());
+						
+						if (sememeVersion.isPresent()) {
+							List<String> _subsetList = new ArrayList<>();
+							_subsetList.add("add"); // Action
+							String subsetName = concept.getConceptDescriptionText();
+							_subsetList.add(sememeVersion.get().value().getString()); // VUID
+							// I'm assuming this will always be 'true' or 'false' - never empty or another identifier
+							String active = Boolean.toString(sememe.isLatestVersionActive(StampCoordinates.getDevelopmentLatestActiveOnly()));
+							_subsetList.add(active); // Active
+							// Just incase it's needed, might as well have it
+							_subsetList.add(concept.getPrimordialUuid().toString()); // UUID
+							// Add to map
+		                    subsets.put(subsetName, _subsetList);
+		                }
+					}
+				});
 			}
-		});*/
+		});
+
+		//System.out.println(Get.conceptService().getConceptChronologyStream().count());
         
-        System.out.println(Get.sememeService().getAssemblageTypes().count());
+        //System.out.println(Get.sememeService().getAssemblageTypes().count());
 		
 		shutdown = true;
 		log.info("Stopping ISAAC");
@@ -239,6 +260,84 @@ public class ExporterConfig {
 		log.info("ISAAC stopped");
 
 	}
-	
 
+	private void testXml() {
+		
+		try {
+			
+			JAXBContext jaxbContext = JAXBContext.newInstance(Terminology.class);
+			Terminology terminology = new Terminology();
+			
+			// Types
+			terminology.setTypes(new Terminology.Types());
+			Terminology.Types.Type _type;
+
+			for (String s : designationTypes.values()) {
+				_type = new Terminology.Types.Type();
+				_type.setKind(KindType.DESIGNATION_TYPE);
+				_type.setName(s);
+				terminology.getTypes().getType().add(_type);
+			}
+			
+			for (String s : propertyTypes.values()) {
+				_type = new Terminology.Types.Type();
+				_type.setKind(KindType.PROPERTY_TYPE);
+				_type.setName(s);
+				terminology.getTypes().getType().add(_type);
+			}
+						
+			for (String s : relationshipTypes.values()) {
+				_type = new Terminology.Types.Type();
+				_type.setKind(KindType.RELATIONSHIP_TYPE);
+				_type.setName(s);
+				terminology.getTypes().getType().add(_type);
+			}
+			
+			// Subsets/Refsets
+			terminology.setSubsets(new Terminology.Subsets());
+			Terminology.Subsets.Subset _subset;
+			
+			for (Map.Entry<String, List<String>> entry : subsets.entrySet()) {
+				_subset = new Terminology.Subsets.Subset();
+				String name = entry.getKey();
+				List<String> al = entry.getValue(); // 0: action, 1: VUID, 2: active, 3: UUID
+				if (al.equals("add")) { 
+					_subset.setAction(ActionType.ADD); 
+				}
+				_subset.setName(name);
+				_subset.setVUID(Long.valueOf(al.get(1)));
+				_subset.setActive(Boolean.parseBoolean(al.get(2)));
+				terminology.getSubsets().getSubset().add(_subset);
+			}
+			
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			//To avoid XXE injections add setFeature
+			dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+	        Document document = db.newDocument();
+	        
+	        Marshaller marshaller = jaxbContext.createMarshaller();
+	        marshaller.marshal(terminology, document);
+	        
+	        TransformerFactory tf = TransformerFactory.newInstance();
+	        //To protect a TransformerFactory against XXE injection add setAttribute
+	        tf.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+	        Transformer t = tf.newTransformer();
+	        DOMSource source = new DOMSource(document);
+	        StreamResult result = new StreamResult(System.out);
+	        t.transform(source, result);
+			
+			/* TODO: We want to use this method, after we have time to adjust
+			 * for Fortify scans and not create any warnings/failures
+			 * 
+			Marshaller marshaller = jaxbContext.createMarshaller();
+	        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+	        marshaller.marshal(terminology, System.out );
+	        */
+	        
+		} catch (Exception e) {
+			// TODO
+		}
+		
+	}
 }
